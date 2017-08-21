36f4796bdd306163a883f92afa6e45f827cabb1e
gpanagiotinos <gpanagiotinos@gmail.com>
2017-08-21 13:55:51 +0300

    TOP MIDDLE BAR

---
+++ templates/page.tpl.php
@@ -0,0 +1,42 @@
+<div id="navbar" role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+<div class="container-fluid" >
+	<ul class="nav navbar-nav navbar-left" >
+	<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-heart"></span>Wishlist</a></li>
+		<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-shopping-cart" ></span> Items</a></li>
+	</ul>
+	<ul class="nav navbar-nav navbar-right" >
+		<li><a href="#" class="custom_fixednav_right_text">Login</a></li>
+		<li><a href="#" class="custom_fixednav_right_text">Register</a></li>
+		<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gr"></span></a></li>
+		<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gb"></span></a></li>
+	</ul>
+</div>
+</div>
+<div class="logo_div">
+	<div class="container">
+		<div class="custom_middle_navbar_row row" >
+			<div class="img_div col-xs-9 col-sm-9 col-md-3 col-lg-3">
+				<a href="#"><img class="img_tag" src="https://www.expresspublishing.co.uk/gr/sites/default/files/logo_header.png" alt=""></a>
+			</div>
+			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 pull-right ">
+				<div class="custom_divinput_group input-group">
+				<input type="text" class="custom_input_search  form-control" placeholder="Search for...">
+				<span class="input-group-btn">
+					<button class="custom_button_search btn btn-default" type="button">Search</button>
+				</span>
+				</div>
+				
+			</div>
+			<div class="custom_middle_menu col-md-9 col-lg-9 pull-right" >
+				<ul class="custom_middle_menu_ul nav navbar-nav navbar-right">
+					<li><a href="">TEACHER'S CORNER</a></li>
+					<li><a href="">STUDENTS</a></li>
+					<li><a href="">CATALOGUE</a></li>
+					<li><a href="">MICROSITES</a></li>
+					<li><a href="">NEWS</a></li>
+					<li><a href="">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
