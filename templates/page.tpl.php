09b7a191f6f7ec9f533c3e61f1817cd5d86ea4d9
gpanagiotinos <gpanagiotinos@gmail.com>
2017-08-22 13:59:43 +0300

    Problem With Login Register float

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,21 +1,33 @@
+<!--fixed navbar -->
 <div id="navbar" role="navigation" class="navbar navbar-inverse navbar-fixed-top">
 <div class="container-fluid" >
-	<ul class="nav navbar-nav navbar-left" >
-	<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-heart"></span>Wishlist</a></li>
-		<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-shopping-cart" ></span> Items</a></li>
-	</ul>
-	<ul class="nav navbar-nav navbar-right" >
-		<li><a href="#" class="custom_fixednav_right_text">Login</a></li>
-		<li><a href="#" class="custom_fixednav_right_text">Register</a></li>
-		<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gr"></span></a></li>
-		<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gb"></span></a></li>
-	</ul>
+	<div class="row">
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<ul class="nav navbar-nav navbar-left">
+			<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-heart"></span>Wishlist</a></li>
+				<li><a href="#" class="custom_fixednav_left_text" ><span class="custom_glyphicon glyphicon glyphicon-shopping-cart" ></span> Items</a></li>
+			</ul>
+		</div>
+		<div class="custom_fixedright_ul col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<ul class="nav navbar-nav navbar-right" >
+				<div class="custom_fixednav_right_links">
+					<li><a href="#" class="custom_fixednav_right_text">Login</a></li>
+					<li><a href="#" class="custom_fixednav_right_text">Register</a></li>
+				</div>
+				<div class="custom_fixednav_right_lang" >
+					<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gr"></span></a></li>
+					<li><a href="#" class="custom_fixednav_right_icon"><span class="flag-icon flag-icon-gb"></span></a></li>
+				</div>
+			</ul>
+		</div>
+	</div>
+	</div>
 </div>
-</div>
+<!-- logo and search bar -->
 <div class="logo_div">
 	<div class="container">
 		<div class="custom_middle_navbar_row row" >
-			<div class="img_div col-xs-9 col-sm-9 col-md-3 col-lg-3">
+			<div class="img_div col-xs-12 col-sm-12 col-md-3 col-lg-3">
 				<a href="#"><img class="img_tag" src="https://www.expresspublishing.co.uk/gr/sites/default/files/logo_header.png" alt=""></a>
 			</div>
 			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 pull-right ">
@@ -27,16 +39,33 @@
 				</div>
 				
 			</div>
-			<div class="custom_middle_menu col-md-9 col-lg-9 pull-right" >
+			<div class="custom_middle_menu hidden-xs hidden-sm col-md-9 col-lg-9 pull-right" >
 				<ul class="custom_middle_menu_ul nav navbar-nav navbar-right">
-					<li><a href="">TEACHER'S CORNER</a></li>
-					<li><a href="">STUDENTS</a></li>
-					<li><a href="">CATALOGUE</a></li>
-					<li><a href="">MICROSITES</a></li>
-					<li><a href="">NEWS</a></li>
-					<li><a href="">CONTACT</a></li>
+					<li><a href="#">TEACHER'S CORNER</a></li>
+					<li><a href="#">STUDENTS</a></li>
+					<li><a href="#">CATALOGUE</a></li>
+					<li><a href="#">MICROSITES</a></li>
+					<li><a href="#">NEWS</a></li>
+					<li><a href="#">CONTACT</a></li>
 				</ul>
 			</div>
+		</div>
+	</div>
+</div>
+<!--main menu bar-->
+<div class="custom_main_menu_container container-fluid">
+	<div class="row">
+		<div id="main_menu" role="navigation" class="custom_main_menu_navbar navbar col-md-12 col-lg-12">
+			<ul class="custom_main_menu nav navbar-nav">
+				<li><a href="#">PRIMARY</a></li>
+				<li><a href="#">SECONDARY</a></li>
+				<li><a href="#">EXAMS</a></li>
+				<li><a href="#">GRAMMAR</a></li>
+				<li><a href="#">SKILLS</a></li>
+				<li><a href="#">RESOURCE BOOKS</a></li>
+				<li><a href="#">READERS</a></li>
+				<li><a href="#">ESP</a></li>
+			</ul>
 		</div>
 	</div>
 </div>
